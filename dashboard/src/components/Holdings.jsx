6cf53a066f4eac910d16e8c54dfbbdd4a799165f
Arpit Pal <arpitpal16112@gmail.com>
2026-07-08 22:07:22 +0530

    Added dummy data to Holdings menu component

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,11 +1,14 @@
 // Here we are creating this 'Holdings' component to be used in the dashboard of our application.
 
 
+// Here this 'holdings' variable is an array of objects, where each object represents a stock or financial instrument that the user currently owns. Each object contains the following properties: 'instrument' (the name of the stock or financial instrument), 'quantity' (the number of shares or units owned), 'avgCost' (the average cost per share or unit), 'ltp' (the last traded price), 'curVal' (the current value of the holding), 'pnl' (the profit or loss for the holding), 'netChg' (the net change in value), and 'dayChg' (the change in value for the day). This data will be used to display the user's holdings in the dashboard.
+import { holdings } from "../data/data.js";
+
 
 export default function Holdings() {
   return (
     <>
-        <h3 className="title">Holdings (13)</h3>
+        <h3 className="title">Holdings ({holdings.length})</h3>
 
         <div className="order-table">
             <table>
@@ -19,6 +22,31 @@ export default function Holdings() {
                     <th>Net chg.</th>
                     <th>Day chg.</th>
                 </tr>
+
+                {/* Here this 'stock' represents each item in the 'holdings' array & 'index' is the index of the current item */}
+                {holdings.map((stock, index) => {
+                    const curValue = stock.price * stock.qty;
+                    const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+                    const profClass = isProfit ? "profit" : "loss";
+                    
+                    // So if this isLoss doesn't then also it will be false as in JavaScript objects If stock.isLoss is undefined (because the property doesn’t exist), the condition evaluates as falsy.
+                    const dayClass = stock.isLoss ? "loss" : "profit";
+
+                    return (
+                        <tr key={index}>
+                            <td>{stock.name}</td>
+                            <td>{stock.qty}</td>
+                            <td>{stock.avg.toFixed(2)}</td>
+                            <td>{stock.price.toFixed(2)}</td>
+                            <td>{curValue.toFixed(2)}</td>
+                            <td className={profClass}>
+                            {(curValue - stock.avg * stock.qty).toFixed(2)}
+                            </td>
+                            <td className={profClass}>{stock.net}</td>
+                            <td className={dayClass}>{stock.day}</td>
+                        </tr>
+                    );
+                })}
             </table>
         </div>
 
---
+++ dashboard/src/data/data.js
@@ -0,0 +1,201 @@
+// Here we are defining a constant variable named 'watchlist' and exporting it for use in other parts of our application. The 'watchlist' variable is an array of objects, where each object represents a stock or financial instrument that the user is interested in monitoring. Each object contains the following properties
+// We also have defined a constant variable named 'holdings' and exporting it for use in other parts of our application. The 'holdings' variable is an array of objects, where each object represents a stock or financial instrument that the user currently owns. Each object contains the following properties
+
+// SO we will use this file to store the dummy data for our application. We will use this data to display the watchlist, holdings, and positions in our dashboard. This data will be used to simulate the real-time data that we would get from an API in a real-world application. By using this dummy data, we can develop and test our application without needing to connect to a live data source. This approach allows us to focus on building the user interface and functionality of our dashboard while ensuring that the components work correctly with the provided data structure.
+// SO we make this as .js file & not .jsx file because we are not creating any React component here. We are just defining some data that will be used in our React components. So we don't need to use JSX syntax here. We can just use plain JavaScript to define our data and export it for use in other parts of our application.
+
+export const watchlist = [
+  {
+    name: "INFY",
+    price: 1555.45,
+    percent: "-1.60%",
+    isDown: true,
+  },
+  {
+    name: "ONGC",
+    price: 116.8,
+    percent: "-0.09%",
+    isDown: true,
+  },
+  {
+    name: "TCS",
+    price: 3194.8,
+    percent: "-0.25%",
+    isDown: true,
+  },
+  {
+    name: "KPITTECH",
+    price: 266.45,
+    percent: "3.54%",
+    isDown: false,
+  },
+  {
+    name: "QUICKHEAL",
+    price: 308.55,
+    percent: "-0.15%",
+    isDown: true,
+  },
+  {
+    name: "WIPRO",
+    price: 577.75,
+    percent: "0.32%",
+    isDown: false,
+  },
+  {
+    name: "M&M",
+    price: 779.8,
+    percent: "-0.01%",
+    isDown: true,
+  },
+  {
+    name: "RELIANCE",
+    price: 2112.4,
+    percent: "1.44%",
+    isDown: false,
+  },
+  {
+    name: "HUL",
+    price: 512.4,
+    percent: "1.04%",
+    isDown: false,
+  },
+];
+
+
+// holdings
+export const holdings = [
+  {
+    name: "BHARTIARTL",
+    qty: 2,
+    avg: 538.05,
+    price: 541.15,
+    net: "+0.58%",
+    day: "+2.99%",
+  },
+  {
+    name: "HDFCBANK",
+    qty: 2,
+    avg: 1383.4,
+    price: 1522.35,
+    net: "+10.04%",
+    day: "+0.11%",
+  },
+  {
+    name: "HINDUNILVR",
+    qty: 1,
+    avg: 2335.85,
+    price: 2417.4,
+    net: "+3.49%",
+    day: "+0.21%",
+  },
+  {
+    name: "INFY",
+    qty: 1,
+    avg: 1350.5,
+    price: 1555.45,
+    net: "+15.18%",
+    day: "-1.60%",
+    isLoss: true,
+  },
+  {
+    name: "ITC",
+    qty: 5,
+    avg: 202.0,
+    price: 207.9,
+    net: "+2.92%",
+    day: "+0.80%",
+  },
+  {
+    name: "KPITTECH",
+    qty: 5,
+    avg: 250.3,
+    price: 266.45,
+    net: "+6.45%",
+    day: "+3.54%",
+  },
+  {
+    name: "M&M",
+    qty: 2,
+    avg: 809.9,
+    price: 779.8,
+    net: "-3.72%",
+    day: "-0.01%",
+    isLoss: true,
+  },
+  {
+    name: "RELIANCE",
+    qty: 1,
+    avg: 2193.7,
+    price: 2112.4,
+    net: "-3.71%",
+    day: "+1.44%",
+  },
+  {
+    name: "SBIN",
+    qty: 4,
+    avg: 324.35,
+    price: 430.2,
+    net: "+32.63%",
+    day: "-0.34%",
+    isLoss: true,
+  },
+  {
+    name: "SGBMAY29",
+    qty: 2,
+    avg: 4727.0,
+    price: 4719.0,
+    net: "-0.17%",
+    day: "+0.15%",
+  },
+  {
+    name: "TATAPOWER",
+    qty: 5,
+    avg: 104.2,
+    price: 124.15,
+    net: "+19.15%",
+    day: "-0.24%",
+    isLoss: true,
+  },
+  {
+    name: "TCS",
+    qty: 1,
+    avg: 3041.7,
+    price: 3194.8,
+    net: "+5.03%",
+    day: "-0.25%",
+    isLoss: true,
+  },
+  {
+    name: "WIPRO",
+    qty: 4,
+    avg: 489.3,
+    price: 577.75,
+    net: "+18.08%",
+    day: "+0.32%",
+  },
+];
+
+
+// positions
+export const positions = [
+  {
+    product: "CNC",
+    name: "EVEREADY",
+    qty: 2,
+    avg: 316.27,
+    price: 312.35,
+    net: "+0.58%",
+    day: "-1.24%",
+    isLoss: true,
+  },
+  {
+    product: "CNC",
+    name: "JUBLFOOD",
+    qty: 1,
+    avg: 3124.75,
+    price: 3082.65,
+    net: "+10.04%",
+    day: "-1.35%",
+    isLoss: true,
+  },
+];
